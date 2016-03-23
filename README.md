45c179ca46d636b3517054d14bda03de64a7f0f9
Ian McKenney <ian.jafool@gmail.com>
2016-03-22 19:39:03 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# This is where I'm going to keep track of coding questions I've been asked
+
+## By Ian McKenney
+
+##### Q1: Write a function that checks if a string is a substring of another string
+
+##### Q2: Write a function that returns the first instance of a non consecutive letter (e.g. "aaaabbbbcccdeeee" returns "d")
+
+##### Q3: Write a function that uses basic compression on a string so that it returns the number of characters followed by the character itself (e.g. "aaabbeeddc" returns "3a2b2e2d1c")
+
+##### Q5: Write functions to build a binary search tree and check if it is a valid binary tree
---
+++ scripts.js
@@ -0,0 +1,149 @@
+
+//Q1
+function checkSubstring(haystack, needle) {
+  if(haystack.toLowerCase().indexOf(needle.toLowerCase()) !== -1) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+//Q2
+function siftConsecutive(letters) {
+  for(var i = 0; i < letters.length; i++) {
+    if(letters.charAt(i + 1) !== letters.charAt(i) && letters.charAt(i - 1) !== letters.charAt(i)) {
+      return letters.charAt(i);
+    }
+  }
+}
+
+
+//Q3
+function basicCompression(letters) {
+  var result = "";
+  var m = 1;
+  for(var i = 0; i < letters.length; i++) {
+    if(letters.charAt(i + 1) === letters.charAt(i)) {
+      m++;
+    } else {
+      result = result + m + letters.charAt(i);
+      m = 1;
+    }
+  }
+  return result;
+}
+
+//build BST prototype for testing
+function Node(value) {
+  this.value = value;
+}
+
+Node.prototype.setLeft = function(left) {
+  this.left = left;
+}
+
+Node.prototype.setRight = function(right) {
+  this.right = right;
+}
+
+function addNode(tree, node) {
+  if(tree) {
+    if(tree.value < node.value) {
+      if(tree.right) {
+        addNode(tree.right, node);
+      } else {
+        tree.setRight(node);
+      }
+    } else {
+      if(tree.left) {
+        addNode(tree.left, node);
+      } else {
+        tree.setLeft(node);
+      }
+    }
+  } else {
+    tree = node;
+  }
+  return tree;
+}
+
+function createBST() {
+    nodeA = new Node(5);
+    nodeB = new Node(12);
+    nodeC = new Node(10);
+    nodeD = new Node(15);
+    nodeE = new Node(20);
+    nodeF = new Node(25);
+    nodeG = new Node(8);
+    nodeH = new Node(3);
+
+    var tree = addNode(tree, nodeA);
+    tree = addNode(tree, nodeB);
+    // tree.setLeft(nodeB);
+    tree = addNode(tree, nodeC);
+    tree = addNode(tree, nodeD);
+    tree = addNode(tree, nodeE);
+    tree = addNode(tree, nodeF);
+    tree = addNode(tree, nodeG);
+    tree = addNode(tree, nodeH);
+}
+
+function printTree(root) {
+  var currentNode = root;
+  if(currentNode.left) {
+    printTree(currentNode.left);
+  }
+
+  console.log(currentNode.value);
+
+  if(currentNode.right) {
+    printTree(currentNode.right);
+  }
+}
+
+//Q4
+function treeToArray(root, array) {
+  var currentNode = root;
+  if(currentNode.left) {
+    treeToArray(currentNode.left, array);
+  }
+
+  array.push(currentNode.value);
+
+  if(currentNode.right) {
+    treeToArray(currentNode.right, array);
+  }
+}
+
+function checkTree() {
+  var nodeArray = [];
+  createBST();
+  treeToArray(nodeA, nodeArray);
+  for(var i = 0; i < nodeArray.length - 1; i++) {
+    if(nodeArray[i] >= nodeArray[i + 1]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+//jQuery
+$(document).ready(function() {
+  $("form#build").submit(function(event) {
+    createBST();
+    printTree(nodeA);
+    event.preventDefault();
+  });
+
+
+  $("form#check").submit(function(event) {
+    var result = checkTree();
+    if(result) {
+      $("div#result").text("true");
+    } else {
+      $("div#result").text("false");
+    }
+
+    event.preventDefault();
+  });
+});
